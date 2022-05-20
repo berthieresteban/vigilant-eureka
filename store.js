87be26e23cf64e15757bedea6b2c64ea93ccf0cf
Esteban Berthier <berthier.est@gmail.com>
2022-05-21 00:25:08 +0200

    flatten if tree

--- store.js
+++ store.js
@@ -1,7 +1,7 @@
 export class DiscountOffer {
   constructor(partnerName, expiresIn, discountInPercent) {
     if (discountInPercent > 50) {
-      throw new Error("Discount must be lower than 50")
+      throw new Error("Discount must be lower than 50");
     }
     this.partnerName = partnerName;
     this.expiresIn = expiresIn;
@@ -13,56 +13,57 @@ export class Store {
   constructor(discountOffers = []) {
     this.discountOffers = discountOffers;
   }
-  updateDiscounts() {
-    for (var i = 0; i < this.discountOffers.length; i++) {
-      if (
-        this.discountOffers[i].partnerName != "Naturalia" &&
-        this.discountOffers[i].partnerName != "Vinted"
-      ) {
-        if (this.discountOffers[i].discountInPercent > 0) {
-          if (this.discountOffers[i].partnerName != "Ilek") {
-            this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent - 1;
-          }
-        }
-      } else {
-        if (this.discountOffers[i].discountInPercent < 50) {
-          this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
-          if (this.discountOffers[i].partnerName == "Vinted") {
-            if (this.discountOffers[i].expiresIn < 11) {
-              if (this.discountOffers[i].discountInPercent < 50) {
-                this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
-              }
-            }
-            if (this.discountOffers[i].expiresIn < 6) {
-              if (this.discountOffers[i].discountInPercent < 50) {
-                this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.discountOffers[i].partnerName != "Ilek") {
-        this.discountOffers[i].expiresIn = this.discountOffers[i].expiresIn - 1;
-      }
-      if (this.discountOffers[i].expiresIn < 0) {
-        if (this.discountOffers[i].partnerName != "Naturalia") {
-          if (this.discountOffers[i].partnerName != "Vinted") {
-            if (this.discountOffers[i].discountInPercent > 0) {
-              if (this.discountOffers[i].partnerName != "Ilek") {
-                this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent - 1;
-              }
-            }
-          } else {
-            this.discountOffers[i].discountInPercent =
-              this.discountOffers[i].discountInPercent - this.discountOffers[i].discountInPercent;
-          }
-        } else {
-          if (this.discountOffers[i].discountInPercent < 50) {
-            this.discountOffers[i].discountInPercent = this.discountOffers[i].discountInPercent + 1;
-          }
-        }
-      }
+  getModificatorSign(offer) {
+    if (["Naturalia", "Vinted"].includes(offer.partnerName)) {
+      return 1;
     }
+    return -1;
+  }
+  getDiscountModificatorValue(offer) {
+    if (offer.partnerName === "Vinted") {
+      if (offer.expiresIn <= 5) {
+        return 3;
+      }
+      if (offer.expiresIn <= 10) {
+        return 2;
+      }
+      return 1;
+    }
+    if (offer.partnerName === "Ilek") {
+      return 0;
+    }
+    if (offer.partnerName === "Naturalia") {
+      return offer.expiresIn > 0 ? 1 : 2;
+    }
+    if (offer.expiresIn <= 0) {
+      return 2;
+    }
+    return 1;
+  }
+  updateDiscounts() {
+    this.discountOffers = this.discountOffers.map(offer => {
+      const modificatorSign = this.getModificatorSign(offer);
+      const modificatorValue = this.getDiscountModificatorValue(offer);
+      const modificator = modificatorValue * modificatorSign;
+
+      if (offer.partnerName != "Ilek") {
+        offer.expiresIn = offer.expiresIn - 1;
+      }
+      if (offer.partnerName === "Vinted" && offer.expiresIn < 0) {
+        offer.discountInPercent = 0;
+        return offer;
+      }
+
+      offer.discountInPercent = offer.discountInPercent + modificator;
+      if (offer.discountInPercent > 50) {
+        offer.discountInPercent = 50;
+      }
+      if (offer.discountInPercent < 0) {
+        offer.discountInPercent = 0;
+      }
+
+      return offer;
+    });
 
     return this.discountOffers;
   }
