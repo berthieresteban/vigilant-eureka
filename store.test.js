680414e734cb956222e35a37bf9e4750efcf090a
Esteban Berthier <berthier.est@gmail.com>
2022-05-21 14:52:15 +0200

    complete tests for 100% coverage

--- store.test.js
+++ store.test.js
@@ -4,6 +4,10 @@ import { DiscountOffer } from "./src/Offers";
 import fs from "fs";
 
 describe("Store", () => {
+  it("should be able to create store without discount", () => {
+    expect(new Store().updateDiscounts()).toEqual([]);
+  });
+
   it("should throw if a discount is > 50", () => {
     expect(() =>
       new Store([new DiscountOffer("test", 10, 60)]).updateDiscounts()
@@ -22,58 +26,16 @@ describe("Store", () => {
     ).toEqual([new DiscountOffer("test", -1, 8)]);
   });
 
-  it("should increase the discount for Naturalia", () => {
+  it("should not decrease the discount under 0", () => {
     expect(
-      new Store([new DiscountOffer("Naturalia", 2, 10)]).updateDiscounts()
-    ).toEqual([new DiscountOffer("Naturalia", 1, 11)]);
+      new Store([new DiscountOffer("test", 10, 0)]).updateDiscounts()
+    ).toEqual([new DiscountOffer("test", 9, 0)]);
   });
 
-  it("should increase the discount twice faster after expiration date for Naturalia", () => {
+  it("should not decrease the discount under 0 even after expiration date", () => {
     expect(
-      new Store([new DiscountOffer("Naturalia", 0, 10)]).updateDiscounts()
-    ).toEqual([new DiscountOffer("Naturalia", -1, 12)]);
-  });
-
-  it("should increase the discount twice faster after expiration date for Naturalia", () => {
-    expect(
-      new Store([new DiscountOffer("Naturalia", 0, 10)]).updateDiscounts()
-    ).toEqual([new DiscountOffer("Naturalia", -1, 12)]);
-  });
-
-  it("should not decrease the discount nor expiresIn for Ilek", () => {
-    expect(
-      new Store([new DiscountOffer("Ilek", 10, 10)]).updateDiscounts()
-    ).toEqual([new DiscountOffer("Ilek", 10, 10)]);
-  });
-
-  it("should increase the discount for Vinted", () => {
-    expect(
-      new Store([new DiscountOffer("Vinted", 20, 10)]).updateDiscounts()
-    ).toEqual([new DiscountOffer("Vinted", 19, 11)]);
-  });
-
-  it("should increase the discount by 2 if 5 < expiresIn <= 10  for Vinted", () => {
-    expect(
-      new Store([new DiscountOffer("Vinted", 10, 10)]).updateDiscounts()
-    ).toEqual([new DiscountOffer("Vinted", 9, 12)]);
-  });
-
-  it("should increase the discount by 3 if expiresIn <= 5  for Vinted", () => {
-    expect(
-      new Store([new DiscountOffer("Vinted", 5, 10)]).updateDiscounts()
-    ).toEqual([new DiscountOffer("Vinted", 4, 13)]);
-  });
-
-  it("should decrease the discount twice fast for BackMarket", () => {
-    expect(
-      new Store([new DiscountOffer("BackMarket", 5, 10)]).updateDiscounts()
-    ).toEqual([new DiscountOffer("BackMarket", 4, 8)]);
-  });
-
-  it("should decrease the discount twice fast after expiration date for BackMarket", () => {
-    expect(
-      new Store([new DiscountOffer("BackMarket", -1, 10)]).updateDiscounts()
-    ).toEqual([new DiscountOffer("BackMarket", -2, 6)]);
+      new Store([new DiscountOffer("test", 0, 0)]).updateDiscounts()
+    ).toEqual([new DiscountOffer("test", -1, 0)]);
   });
 
   it("should get the exact same result for the provided sample data", () => {
@@ -92,5 +54,121 @@ describe("Store", () => {
       log.push(JSON.stringify(store.updateDiscounts()));
     }
     expect(log.toString()).toEqual(result);
+  });
+});
+
+describe("Naturalia", () => {
+  it("should increase the discount", () => {
+    expect(
+      new Store([new DiscountOffer("Naturalia", 2, 10)]).updateDiscounts()
+    ).toEqual([new DiscountOffer("Naturalia", 1, 11)]);
+  });
+
+  it("should increase the discount twice faster after expiration date", () => {
+    expect(
+      new Store([new DiscountOffer("Naturalia", 0, 10)]).updateDiscounts()
+    ).toEqual([new DiscountOffer("Naturalia", -1, 12)]);
+  });
+
+  it("should increase the discount twice faster after expiration date", () => {
+    expect(
+      new Store([new DiscountOffer("Naturalia", 0, 10)]).updateDiscounts()
+    ).toEqual([new DiscountOffer("Naturalia", -1, 12)]);
+  });
+
+  it("should not increase the discount beyond 50", () => {
+    expect(
+      new Store([new DiscountOffer("Naturalia", 10, 50)]).updateDiscounts()
+    ).toEqual([new DiscountOffer("Naturalia", 9, 50)]);
+  });
+
+  it("should not increase the discount beyond 50 even after expiration date", () => {
+    expect(
+      new Store([new DiscountOffer("Naturalia", 0, 50)]).updateDiscounts()
+    ).toEqual([new DiscountOffer("Naturalia", -1, 50)]);
+  });
+});
+
+describe("Ilek", () => {
+  it("should not decrease the discount nor expiresIn", () => {
+    expect(
+      new Store([new DiscountOffer("Ilek", 10, 10)]).updateDiscounts()
+    ).toEqual([new DiscountOffer("Ilek", 10, 10)]);
+  });
+
+  it("should not decrease the discount nor expiresIn even after expiration date", () => {
+    expect(
+      new Store([new DiscountOffer("Ilek", -1, 10)]).updateDiscounts()
+    ).toEqual([new DiscountOffer("Ilek", -1, 10)]);
+  });
+});
+
+describe("Vinted", () => {
+  it("should increase the discount", () => {
+    expect(
+      new Store([new DiscountOffer("Vinted", 20, 10)]).updateDiscounts()
+    ).toEqual([new DiscountOffer("Vinted", 19, 11)]);
+  });
+
+  it("should increase the discount by 2 if 5 < expiresIn <= 10", () => {
+    expect(
+      new Store([new DiscountOffer("Vinted", 10, 10)]).updateDiscounts()
+    ).toEqual([new DiscountOffer("Vinted", 9, 12)]);
+  });
+
+  it("should increase the discount by 3 if expiresIn <= 5", () => {
+    expect(
+      new Store([new DiscountOffer("Vinted", 5, 10)]).updateDiscounts()
+    ).toEqual([new DiscountOffer("Vinted", 4, 13)]);
+  });
+
+  it("should not increase the discount beyond 50", () => {
+    expect(
+      new Store([new DiscountOffer("Vinted", 20, 50)]).updateDiscounts()
+    ).toEqual([new DiscountOffer("Vinted", 19, 50)]);
+  });
+
+  it("should not increase the discount beyond 50 even with expiresIn < 10", () => {
+    expect(
+      new Store([new DiscountOffer("Vinted", 10, 50)]).updateDiscounts()
+    ).toEqual([new DiscountOffer("Vinted", 9, 50)]);
+  });
+
+  it("should not increase the discount beyond 50 even with expiresIn < 5", () => {
+    expect(
+      new Store([new DiscountOffer("Vinted", 5, 50)]).updateDiscounts()
+    ).toEqual([new DiscountOffer("Vinted", 4, 50)]);
+  });
+
+  it("should set the discount to 0 after expiration date", () => {
+    expect(
+      new Store([new DiscountOffer("Vinted", 0, 50)]).updateDiscounts()
+    ).toEqual([new DiscountOffer("Vinted", -1, 0)]);
+  });
+});
+
+describe("BackMarket", () => {
+  it("should decrease the discount twice fast", () => {
+    expect(
+      new Store([new DiscountOffer("BackMarket", 5, 10)]).updateDiscounts()
+    ).toEqual([new DiscountOffer("BackMarket", 4, 8)]);
+  });
+
+  it("should decrease the discount twice fast after expiration date", () => {
+    expect(
+      new Store([new DiscountOffer("BackMarket", -1, 10)]).updateDiscounts()
+    ).toEqual([new DiscountOffer("BackMarket", -2, 6)]);
+  });
+
+  it("should not decrease the discount under 0", () => {
+    expect(
+      new Store([new DiscountOffer("BackMarket", 10, 0)]).updateDiscounts()
+    ).toEqual([new DiscountOffer("BackMarket", 9, 0)]);
+  });
+
+  it("should not decrease the discount under 0 even after expiration date", () => {
+    expect(
+      new Store([new DiscountOffer("BackMarket", 0, 0)]).updateDiscounts()
+    ).toEqual([new DiscountOffer("BackMarket", -1, 0)]);
   });
 });
